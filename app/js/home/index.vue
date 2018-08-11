707fa93e04e928ab61547157d2dd64665c19f114
xuyuexiong <530969335@qq.com>
2018-08-11 20:16:53 +0800

    项目构建

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -1,3 +1,17 @@
-<template>
-    
+<template lang="html">
+    <div class="home">
+        <h1>home</h1>
+    </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "../../css/reset.scss";
+.home {
+  color: red;
+  font-size: 80px;
+}
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
         app: './app/js/main.js'
     },
-    devServer:{
-        contentBase: path.join(_dirname,"dist"),
+    devServer: {
+        contentBase: path.join(_dirname, "dist"),
         compress: true,
         port: 9000
     },
@@ -21,7 +23,17 @@ module.exports = {
             loader: 'style-loader!css-loader!scss-loader'
         }]
     },
-    plugins: [],
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Development'
+        })
+    ],
+    resolve:{
+        alias:{
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
     output: {
         filename: '[name].min.js',
         path: path.resolve(_dirname, 'dist')
