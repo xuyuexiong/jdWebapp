2a1d369622d50472bedf2a1adfa76780616eb5b0
xuyuexiong <530969335@qq.com>
2018-08-20 20:14:11 +0800

    css模块化

--- app/js/router/index.js
+++ app/js/router/index.js
@@ -1,6 +1,6 @@
-import Vue from "vue"
-import Router from "vue-router"
-import Home from "../home/index.vue"
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "../home/index.vue";
 
 Vue.use(Router)
 
