26623507c6ce1f8aff7cd0845b0ef59718b1e52f
xuyuexiong <530969335@qq.com>
2018-08-21 20:21:17 +0800

    公共组件创建

---
+++ app/js/core/panel.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+    <section class="panel">
+        
+    </section>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style lang="scss">
+	@import "../../css/element.scss";
+	
+</style>
