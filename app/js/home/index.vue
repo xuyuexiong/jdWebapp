8fdb547bd3f70bd25440739b722a956e80dc9882
xuyuexiong <530969335@qq.com>
2018-08-24 17:20:52 +0800

    解决viewport问题；slider组件创建；

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -1,14 +1,17 @@
 <template>
     <div>
         <Heador />
+        <Slider />
     </div>
 </template>
 
 <script>
 import Heador from '../public/header.vue'
+import Slider from './hslider.vue'
 export default {
   components:{
     Heador,
+    Slider
   }
 }
 </script>
