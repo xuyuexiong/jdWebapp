d775ecd1a6d9684a6a07a3ac33bb9659ffa76ce6
xuyuexiong <530969335@qq.com>
2018-08-28 20:17:27 +0800

    理财精选组件编写

---
+++ app/js/home/money.vue
@@ -0,0 +1,9 @@
+<template>
+    <Panel :class="$style.panel" title="理财精选">
+        <section :class="$style.content">
+            <router-link :to="{ name: 'home' }">
+                <img src="//img12.360buyimg.com/jrpmobile/jfs/t12721/42/2497849749/26333/ec584be4/5a421756N2416c88f.png?width=750&height=280" alt="">
+            </router-link>
+        </section>
+    </Panel>
+</template>
