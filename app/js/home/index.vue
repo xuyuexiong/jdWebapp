00751cfd6f509b153b310d4f756bfbd0eb19bcd1
xuyuexiong <530969335@qq.com>
2018-08-28 20:16:56 +0800

    理财精选构建

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -2,16 +2,22 @@
     <div>
         <Heador />
         <Slider />
+        <Novice />
+        <Borrow />
     </div>
 </template>
 
 <script>
-import Heador from '../public/header.vue'
-import Slider from './hslider.vue'
+import Heador from '../public/header.vue';
+import Slider from './hslider.vue';
+import Novice from './novice.vue';
+import Borrow from './borrow.vue';
 export default {
   components:{
     Heador,
-    Slider
+    Slider,
+    Novice,
+    Borrow
   }
 }
 </script>
