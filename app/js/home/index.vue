0add8760b0e03b6e7fbf8adc88b320cdba4b1b1a
xuyuexiong <530969335@qq.com>
2018-08-31 17:32:27 +0800

    完成生活服务组件

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -6,6 +6,7 @@
         <Borrow />
         <Money />
         <Product />
+        <Life />
     </div>
 </template>
 
@@ -16,6 +17,7 @@ import Novice from './novice.vue';
 import Borrow from './borrow.vue';
 import Money from './Money.vue';
 import Product from './product.vue';
+import Life from './life.vue';
 export default {
   components:{
     Heador,
@@ -23,7 +25,8 @@ export default {
     Novice,
     Borrow,
     Money,
-    Product
+    Product,
+    Life
   }
 }
 </script>
