e12b66cfb6146bfcb3ce2dfb186028e8d5ea70f7
xuyuexiong <530969335@qq.com>
2018-09-04 18:00:49 +0800

    理财路由配置、组件编写

---
+++ app/js/money/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Heador/>
+        <Slider/>
+        <Navbar/>
+        <Footor/>
+    </div>
+</template>
+
+<script>
+import Heador from "../public/header.vue"
+import Footor from "../public/footer.vue"
+import Navbar from "../public/navbar.vue"
+import Slider from "./mslider.vue"
+
+export default {
+    components: {
+        Heador,
+        Slider,
+        Navbar,
+        Footor,
+    },
+}
+</script>
