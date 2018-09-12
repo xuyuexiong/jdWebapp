bda164d230db0d546c74dba967966af545c98d86
xuyuexiong <530969335@qq.com>
2018-09-12 19:13:54 +0800

    创建白条组件

---
+++ app/js/ious/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <Heador/>
+        <Slider/>
+        <Navbar/>
+        <Footor/>
+    </div>
+</template>
+
+<script>
+import Heador from "../public/header.vue";
+import Footor from "../public/footer.vue";
+import Navbar from "../public/navbar.vue";
+import Slider from "./islider.vue";
+
+export default {
+  components: {
+    Heador,
+    Slider,
+    Navbar,
+    Footor,
+    
+  }
+};
+</script>
+
--- app/js/router/index.js
+++ app/js/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue"
 import Router from "vue-router"
 import Home from "../home/index.vue"
 import Money from "../Money/index.vue"
+import Ious from "../ious/index.vue"
 import "../../css/reset.scss"
 
 Vue.use(Router)
@@ -17,6 +18,10 @@ export default new Router({
             path: "/money",
             name: "money",
             component: Money,
-        },
+        }, {
+            path: "/ious",
+            name: "ious",
+            component: Ious,
+        }
     ],
 })
